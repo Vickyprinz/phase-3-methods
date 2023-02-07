440c7bcc72ed41f8f1498fd633bbfb8d2e7e0199
git_Vickyprinz <kariukivictor315@gmail.com>
2023-02-07 13:02:42 +0300

    passed test

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+  end
+  greet
+
+#   def greet_with_default(name = "Naureen")
+#     puts "Hello, #{name}!"
+#   end
+#   greet_with_default
+
+ def greet_with_default name = 'programmer'
+    puts "Hello, #{name}!"
+    end
+
+    def add(num1, num2)
+        return 2 + 5
+      end
+
+      def halve number
+    
+        if number.class != Integer
+            return nil
+          end
+          number/2
+        end
